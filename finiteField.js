990ffafb51ae2a92e6356006652145e8ca1f790b
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2021-01-24 14:57:13 +0800

    changed ES6 naming

--- finiteField.js
+++ finiteField.js
@@ -1,4 +1,6 @@
-import * as galois from '@guildofweavers/galois';
+// import * as galois from '@guildofweavers/galois';
+const galois = require("@guildofweavers/galois");
+
 
 // create a prime field with a large modulus
 const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
@@ -6,3 +8,7 @@ const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
 const a = field.rand();     // generate a random field element
 const b = field.rand();     // generate another random element
 const c = field.exp(a, b);  // do some math
+
+console.log(a)
+console.log(b)
+console.log(c)
