058e77f7674a925ab65d76604fb3b621f090b0ad
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2021-01-24 14:43:03 +0800

    added galois field framework

---
+++ finiteField.js
@@ -0,0 +1,8 @@
+import * as galois from '@guildofweavers/galois';
+
+// create a prime field with a large modulus
+const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
+
+const a = field.rand();     // generate a random field element
+const b = field.rand();     // generate another random element
+const c = field.exp(a, b);  // do some math
