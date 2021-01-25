fb811902015d7d23e539ccf00f4d6e2bed3c9ac9
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2021-01-26 02:23:48 +0800

    tweaked some code for galois field

--- finiteField.js
+++ finiteField.js
@@ -1,13 +1,12 @@
 // import * as galois from '@guildofweavers/galois';
 const galois = require("@guildofweavers/galois");
 
-
 // create a prime field with a large modulus
-const field = galois.createPrimeField(2n ** 256n - 351n * 2n ** 32n + 1n);
+const field = galois.createPrimeField(5n);
 
 const a = field.rand();     // generate a random field element
 const b = field.rand();     // generate another random element
-const c = field.exp(a, b);  // do some math
+const c = field.div(1n,2n)
 
 console.log(a)
 console.log(b)
