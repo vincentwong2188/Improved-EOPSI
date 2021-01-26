0a0fa4f8a3f4c7f4fe3c27c2411ed69f6c9db17a
Collin Lim <collinlimtj@gmail.com>
2021-01-26 12:14:04 +0800

    updated ports and configs

--- cloud-app/cloud-server/lib/config/database.ts
+++ cloud-app/cloud-server/lib/config/database.ts
@@ -1,7 +1,6 @@
 
 export const user = 'postgres'
-export const password = 'limtj123'
-export const database = 'rozzby'
-export const host = 'rozzby-db.ceblb2icynpa.ap-southeast-1.rds.amazonaws.com'
-// export const host = 'db-12'
+export const password = ''
+export const database = ''
+export const host = ''
 export const port = 5432
